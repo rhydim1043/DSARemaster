483b4419350ab595b358a1fa13e2888a6f7fa6de
rhydim1043 <rhydim1043.cse19@chitkara.edu.in>
2023-03-09 18:52:10 +0530

    Linked List and Valid Parenthesis

---
+++ src/com/company/DSImplementation/LinkedList/LinkedList.java
@@ -0,0 +1,11 @@
+package com.company.DSImplementation.LinkedList;
+
+public class Node {
+    int val;
+    Node next;
+    Node (int val)
+    {
+        this.val = val;
+        next=null;
+    }
+}
