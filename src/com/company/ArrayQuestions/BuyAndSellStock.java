aad7957a23c6f221a23497f9dad805d8a25b8d26
rhydim1043 <rhydim1043.cse19@chitkara.edu.in>
2023-03-13 03:03:46 +0530

    Merge 2 Sorted lists

--- src/com/company/ArrayQuestions/BuyAndSellStock.java
+++ src/com/company/ArrayQuestions/BuyAndSellStock.java
@@ -1,2 +1,17 @@
-package com.company.ArrayQuestions;public class BuyAndSellStock {
+package com.company.ArrayQuestions;
+
+public class BuyAndSellStock {
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+        System.out.println(maxProfit(arr));
+    }
+    public static int maxProfit(int[] arr) {
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE, profit=0;
+        for(int i : arr)
+        {
+            if(min>i) min = i;
+            if(max < (i-min)) max = i-min;
+        }
+        return max;
+    }
 }
