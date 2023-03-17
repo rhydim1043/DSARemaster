46728ff2d044eef69c1e0d7b6dd70e9a7668526b
Rehan LNU <rhydim1043.cse19@chitkara.edu.in>
2023-03-17 17:49:56 +0530

    Flood fill and insert intrval

---
+++ src/com/company/DSImplementation/LinkedList/Tree.java
@@ -0,0 +1,7 @@
+package com.company.DSImplementation.LinkedList;
+import com.company.GenericClasses.Node;
+public class Tree {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/company/Matrix/FloodFill.java
+++ src/com/company/Matrix/FloodFill.java
@@ -12,18 +12,20 @@ public class FloodFill {
             System.out.println();
         }
     }
-    public static int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
-        int color = image[sr][sc];
-        if (color != newColor) dfs(image, sr, sc, color, newColor);
+    public static int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] == color) return image;
+        int oldColor = image[sr][sc];
+        dfs(image, sr, sc, oldColor, color);
         return image;
     }
-    public static void dfs(int[][] image, int r, int c, int color, int newColor) {
-        if (image[r][c] == color) {
-            image[r][c] = newColor;
-            if (r >= 1) dfs(image, r-1, c, color, newColor);
-            if (c >= 1) dfs(image, r, c-1, color, newColor);
-            if (r+1 < image.length) dfs(image, r+1, c, color, newColor);
-            if (c+1 < image[0].length) dfs(image, r, c+1, color, newColor);
-        }
+    public static void dfs(int[][] image, int sr, int sc,int oldColor, int color){
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=oldColor)
+            return;
+        image[sr][sc] = color;
+        dfs(image, sr+1,sc,oldColor,color);
+        dfs(image, sr-1,sc,oldColor,color);
+        dfs(image, sr,sc+1,oldColor,color);
+        dfs(image, sr,sc-1,oldColor,color);
     }
 }
+
