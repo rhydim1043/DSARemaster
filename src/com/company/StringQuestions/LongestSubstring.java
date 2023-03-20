a47a9b2d0b3771bd47f553b580ded0b0aa88b2bc
Rehan LNU <rhydim1043.cse19@chitkara.edu.in>
2023-03-20 12:57:32 +0530

    Filler

---
+++ src/com/company/StringQuestions/LongestSubstring.java
@@ -0,0 +1,15 @@
+package com.company.StringQuestions;
+
+public class LongestSubstring {
+    public static void main(String[] args) {
+        String str = "pwwkew";
+        System.out.println(lengthOfLongestSubstring(str));
+
+    }
+    public static int lengthOfLongestSubstring(String str)
+    {
+        int max=0;
+
+        return max;
+    }
+}
