da009d09836cbfa75400093a1921705ff85ca1cf
Rehan LNU <rhydim1043.cse19@chitkara.edu.in>
2023-03-22 15:18:11 +0530

    AdList Declaration

---
+++ src/com/company/DSImplementation/Graphs/Undirected/AdjacencyList.java
@@ -0,0 +1,16 @@
+package com.company.DSImplementation.Graphs.Undirected;
+
+import java.util.ArrayList;
+
+public class AdjacencyList {
+    public static void main(String[] args) {
+        int[][] graphInput = {{5,6},{1,2},{1,3},{5,2},{4,5},{4,2},{3,4}};
+
+    }
+    public static ArrayList<ArrayList<Integer>> adjacencyList(int[][] inputMat)
+    {
+        ArrayList<ArrayList<Integer>> adList = new ArrayList<>();
+
+        return adList;
+    }
+}
